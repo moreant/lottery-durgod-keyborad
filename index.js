9c86e8e6842305650e5ff74c918d6c405cff0819
moreant <moreant@foxmail.com>
2021-03-31 22:48:01 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const pool = [
+  // 请从这里按照格式添加你的微信号
+  { vxNumber: 'moreant' },
+]
+
+const randomNumber = Math.floor(Math.random() * (pool.length))
+const res = pool[randomNumber].vxNumber
+console.log(res);
